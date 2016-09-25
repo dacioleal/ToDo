3973d6b480f5c124fa4600086dd35a6e8fe88408
Dacio Leal <dacioleal@gmail.com>
2016-09-25 11:46:39 +0200

    Added TableView in mainViewController

---
+++ ToDo/ItemListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ItemListViewController.swift
+//  ToDo
+//
+//  Created by Dacio Leal Rodriguez on 25/9/16.
+//  Copyright © 2016 Dacio Leal Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDoTests/ItemListViewControllerTests.swift
@@ -0,0 +1,32 @@
+//
+//  ItemListViewControllerTests.swift
+//  ToDo
+//
+//  Created by Dacio Leal Rodriguez on 25/9/16.
+//  Copyright © 2016 Dacio Leal Rodriguez. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo
+
+class ItemListViewControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func test_TableViewIsNotNilAfterViewDidLoad() {
+        /*
+        let storyBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let sut = storyBoard.instantiateViewController(withIdentifier: "ItemListViewController") as! ItemListViewController
+        _ = sut.view
+        XCTAssertNotNil(sut.tableView, "tableview should not be nil")
+ */
+    }
+}
