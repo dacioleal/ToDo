8e4448aeb28f18bac014eaa49a5620bda0cd5f3c
dacioleal <dacioleal@gmail.com>
2016-10-08 20:59:45 +0200

    Added DetailViewController to display item info

---
+++ ToDo/DetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DetailViewController.swift
+//  ToDo
+//
+//  Created by Dacio Leal Rodriguez on 8/10/16.
+//  Copyright © 2016 Dacio Leal Rodriguez. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let dateFormatter: DateFormatter = {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter
+    }()
+    
+    var itemInfo: (ItemManager, Int)?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let itemInfo = itemInfo else { return }
+        let item = itemInfo.0.itemAtIndex(itemInfo.1)
+        
+        titleLabel.text = item.title
+        locationLabel.text = item.location?.name
+        descriptionLabel.text = item.itemDescription
+        
+        if let timestamp = item.timestamp {
+            
+            let date = Date(timeIntervalSince1970: timestamp)
+            dateLabel.text = dateFormatter.string(from: date)
+        }
+        
+        if let coordinate = item.location?.coordinate {
+            
+            let region = MKCoordinateRegionMakeWithDistance(coordinate, 100, 100)
+            mapView.region = region
+        }
+    }
+    
+}
